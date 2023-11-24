f54c6a4701380ee49f9ed13638cc15bf95b2a67f
Mike Fulton <mikefultonpersonal@gmail.com>
2023-11-24 16:48:48 -0500

    re-submit comma fix

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -18,7 +18,6 @@ node('linux')
                         extensions: [],
                         userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/groffport.git"]]])
   }
-
   stage('Build') {
     build job: 'Port-Pipeline', parameters: 
       [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/groffport.git'), 
